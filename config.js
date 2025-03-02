d5e7e78f75b5376ec098b048f0ef52bcbd8c7843
Ayodele David <githubworker5@gmail.com>
2025-03-02 07:27:29 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Created By King Gabimaru 
+global.owner = ['2349116057855'] // owner number
+global.connect = true // Ubah ke false jika ingin mengkoneksikan bot menggunakan QrCode
+global.url = "https://t.me/Gabimaru_Solos"
+global.url2 = "https://t.me/GabimaruTech"
+global.packname = `𝐍𝐨 𝐄𝐬𝐂𝐚𝐩𝐞`
+global.author = "𝐆𝐚𝐛𝐢𝐦𝐚𝐫𝐮"
+// V1 is a menu visible to android only
+// V2 is a video menu visible to both ios and android 
+// V3 is a Call sequence menu that is stylish and fun 🕊️
+global.typemenu = 'v1'
