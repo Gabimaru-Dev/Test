4720fa492b2deb930d6aad1314fc26da635ec59f
Ayodele David <githubworker5@gmail.com>
2025-05-04 12:56:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Created By King Gabimaru 
 global.owner = ['2349116057855'] // owner number
 global.connect = true // Ubah ke false jika ingin mengkoneksikan bot menggunakan QrCode
-global.url = "https://t.me/Gabimaru_Solos"
-global.url2 = "https://t.me/GabimaruTech"
+global.url = "https://t.me/ayokunledavid"
+global.url2 = "https://t.me/lonelydeveloper"
 global.packname = `𝐍𝐨 𝐄𝐬𝐂𝐚𝐩𝐞`
 global.author = "𝐆𝐚𝐛𝐢𝐦𝐚𝐫𝐮"
 // V1 is a menu visible to android only
